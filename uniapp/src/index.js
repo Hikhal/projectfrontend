f2fd676b3134c1195a94519a721b12a1a5c111c2
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-29 18:44:41 -0400

    redux store configured.

--- uniapp/src/index.js
+++ uniapp/src/index.js
@@ -5,13 +5,31 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'react-redux'
 import { applyMiddleware } from 'redux';
-import ThunkMiddleware  from 'redux-thunk'; // allows for using async func
+/*
+ * The thunkmiddleware allows you to write action creators that return a function instead of an action.
+ * The thunk can be used to delay the dispatch of an action, or to dispatch only if certain conditions are met.
+ */
+import ThunkMiddleware  from 'redux-thunk'; 
 import axios from 'axios';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { createLogger } from 'redux-logger';
 
+// * middleware in redux is a way to increase functionality of redux: e.g talking to an asynchronous API, loging etc.
+
+const logger = createLogger({collapsed: true})
 const middleware = composeWithDevTools(
-  applyMiddleware(ThunkMiddleware.withExtraArgument({axios}))
+  applyMiddleware(ThunkMiddleware.withExtraArgument({axios}), logger)
 )
+
+/**
+ * - create a new Redux store that holds the complete state tree of our uniapp. 
+ * - It needs a reducer function as the first argument, and optionally you can pass enhancer as the second argument.
+ * - In this case, rootReducer is passed as the reducer, and middleware
+ * - (which includes the thunk and logging middleware, properly set up for use with DevTools)
+ * - is passed as the enhancer. 
+ */
+const store = createStore(rootReducer, middleware)
+
 
 
 
