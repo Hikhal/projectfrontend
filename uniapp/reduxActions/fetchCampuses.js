531d770a41f6b0f6442bd2f318399052e39f87f8
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-29 19:05:09 -0400

    fetch all campuses action created

---
+++ uniapp/reduxActions/fetchCampuses.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+// creating action
+const actiontype = 'FETCHCAMPUSES'
+
+export const fetchAllCampuses = (payload) => {
+    // console logging for testing
+    console.log("fetching all campuses action")
+    return {
+        type: actiontype,
+        payload: payload
+    }
+}
+
+/**
+ * The function returns an asynchronous function that takes a dispatch parameter.
+ *  This function is the actual thunk function that will be executed when fetchAllCampusesThunk is invoked.
+ */
+
+export const fetchAllCampusesThunk = () => {
+    const apikey = ''
+    return async (dispatch) => {
+        try {
+            const datalist = await axios.get(api)
+            dispatch(fetchAllCampuses(datalist.data))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
