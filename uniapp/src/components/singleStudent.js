762e83efb38deb03a936487c9dae586aa154011a
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-01 19:12:45 -0400

    id sent as param to server, student object successfully returning

--- uniapp/src/components/singleStudent.js
+++ uniapp/src/components/singleStudent.js
@@ -1,8 +1,27 @@
 import React, {useEffect, useState} from "react";
 import { UseSelector } from "react-redux/es/hooks/useSelector";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const SingleStudentInfo = () => {
+    const {id} = useParams()
+    //console.log({id})
+
+    const [studentInfo, setStudentInfo] = useState("")
+    // useEffect
+
+    useEffect(()=>{
+        async function getStudentInfo(){
+            try {
+                const info = await axios.get(`http://localhost:8080/api/students/${id}`)
+                console.log("student 2 info",info.data)
+                setStudentInfo(info.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getStudentInfo()
+    }, [id])
     return (
         <div>
              <h1>single students here</h1>
@@ -10,4 +29,4 @@ const SingleStudentInfo = () => {
     )
 }
 
-export default SingleStudentInfo
+export default SingleStudentInfo 
