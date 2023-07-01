0bfbc30816b26be341282239661f501220a1412f
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-01 17:01:38 -0400

    api call to server made, console logged the object returned --> campus id passed as param

--- uniapp/src/components/singleCampus.js
+++ uniapp/src/components/singleCampus.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 // This is a functional component for displaying individual campus information
 const SingleCampusInfo = () => {
@@ -10,6 +11,22 @@ const SingleCampusInfo = () => {
 
     // Now that the component has access to the campus id,
     // the api call to the server would be made in order to display the info of the campus.
+
+    const [campus, setCampus] = useState("")
+
+    useEffect(()=>{
+        async function getCamp(){
+            try {
+                const campInfo = await axios.get(`http://localhost:8080/api/campuses/${id}`)
+                console.log(campInfo.data)
+                setCampus(campInfo.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getCamp()
+
+    } ,[id])
 
     return (
       <div>
