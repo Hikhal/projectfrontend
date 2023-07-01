8f1cd1a7b893a1bf118571370c05bc00f03398fc
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-01 19:01:36 -0400

    component file created, linked to allStudents and route provided in App.js

---
+++ uniapp/src/components/singleStudent.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
+import axios from "axios";
+
+const SingleStudentInfo = () => {
+    return (
+        <div>
+             <h1>single students here</h1>
+        </div>
+    )
+}
+
+export default SingleStudentInfo
