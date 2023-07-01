6b7bed298c2cced63fbdd57c57681de088faa3f7
Ghulam Nabi <ghualmnabia75@gmail.com>
2023-07-01 19:31:56 -0400

    AllStudent.js - Student card is Updated

--- uniapp/src/components/allStudents.js
+++ uniapp/src/components/allStudents.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { useSelector} from 'react-redux'
-import { fetchAllStudentsThunk } from '../reduxActions/fetchStudents'
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { fetchAllStudentsThunk } from '../reduxActions/fetchStudents';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 const AllStudents = () => {
-  
-    const listOfStudents = useSelector(state => state.getStudents )
-    console.log(listOfStudents)
-    return (
-      <div className="card-container">
-        {listOfStudents.map((item) => (
+  const listOfStudents = useSelector((state) => state.getStudents);
+  const listOfCampuses = useSelector((state) => state.getCampuses);
+
+  return (
+    <div className="card-container">
+      {listOfStudents.map((item) => {
+        // Find the corresponding campus for the student
+        const campus = listOfCampuses.find((campus) => campus.id === item.campusId);
+
+        return (
           <div key={item.id} className="student-card">
             <img src={item.image} alt="Student" className="student-image" />
             <span className="student-info">
@@ -21,24 +26,18 @@ const AllStudents = () => {
                 <span className="info-value">{item.lastName}</span>
               </div>
               <div className="info-item">
-                <span className="info-label">Address:</span>
-                <span className="info-value">{item.address}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">Email:</span>
-                <span className="info-value">{item.email}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">GPA:</span>
-                <span className="info-value">{item.gpa}</span>
+                <span className="info-label">Campus:</span>
+                <span className="info-value">{campus ? campus.name : 'N/A'}</span>
               </div>
             </span>
           </div>
-        ))}
-        <Link to="/addstudent" className='addstudent-link'>Add Student</Link>
-      </div>
-    );
-    
-}
+        );
+      })}
+      <Link to="/addstudent" className="addstudent-link">
+        Add Student
+      </Link>
+    </div>
+  );
+};
 
 export default AllStudents;
