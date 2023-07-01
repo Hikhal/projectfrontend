552180f38b9e8af705b365ec352cb2ac2afcce6e
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-01 19:24:50 -0400

    student info (except campus name) rendered

--- uniapp/src/components/singleStudent.js
+++ uniapp/src/components/singleStudent.js
@@ -14,7 +14,6 @@ const SingleStudentInfo = () => {
         async function getStudentInfo(){
             try {
                 const info = await axios.get(`http://localhost:8080/api/students/${id}`)
-                console.log("student 2 info",info.data)
                 setStudentInfo(info.data)
             } catch (error) {
                 console.error(error)
@@ -22,9 +21,22 @@ const SingleStudentInfo = () => {
         }
         getStudentInfo()
     }, [id])
+
+    // get the campus the student is associated with.
     return (
         <div>
-             <h1>single students here</h1>
+             <img src={studentInfo.image} alt="Student"/>
+             <br></br>
+             <p1> <strong>First Name:</strong>{studentInfo.firstName}</p1>
+             <br></br>
+             <p1> <strong>Last Name:  </strong>{studentInfo.lastName}</p1>
+             <br></br>
+             <p1> <strong>Address: </strong>{studentInfo.address}</p1>
+             <br></br>
+             <p1> <strong>Email:</strong>{studentInfo.email}</p1>
+             <br></br>
+             <p1> <strong>GPA:</strong> {studentInfo.gpa}</p1>
+             <br></br>
         </div>
     )
 }
