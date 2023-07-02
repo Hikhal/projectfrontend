8a51bfe09ae05cf39dfda8307eb985cefd49f199
Ghulam Nabi <ghualmnabia75@gmail.com>
2023-07-01 20:09:21 -0400

    AllStudent.js have new updates

--- uniapp/src/components/allStudents.js
+++ uniapp/src/components/allStudents.js
@@ -8,35 +8,44 @@ const AllStudents = () => {
   const listOfCampuses = useSelector((state) => state.getCampuses);
 
   return (
+    <div>
+      <header className='student-header'>
+        <h1>All students</h1>
+      </header>
+      
     <div className="card-container">
       {listOfStudents.map((item) => {
         // Find the corresponding campus for the student
         const campus = listOfCampuses.find((campus) => campus.id === item.campusId);
 
         return (
-          <div key={item.id} className="student-card">
-            <img src={item.image} alt="Student" className="student-image" />
-            <span className="student-info">
-              <div className="info-item">
-                <span className="info-label">First Name:</span>
-                <span className="info-value">{item.firstName}</span>
-              </div>
-              <div className="info-item">
-                <span className="info-label">Last Name:</span>
-                <span className="info-value">{item.lastName}</span>
-              </div>
-              {/* Display the Campus name, if a student is not found is will display "N/A" */}
-              <div className="info-item">
-                <span className="info-label">Campus:</span>
-                <span className="info-value">{campus ? campus.name : 'N/A'}</span>
-              </div>
-            </span>
-          </div>
+          
+            <div key={item.id} className="student-card">
+
+              <img src={item.image} alt="Student" className="student-image" />
+              <span className="student-info">
+                <div className="info-item">
+                  <span className="info-label">First Name:</span>
+                  <span className="info-value">{item.firstName}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-label">Last Name:</span>
+                  <span className="info-value">{item.lastName}</span>
+                </div>
+                {/* Display the Campus name, if a student is not found is will display "N/A" */}
+                <div className="info-item">
+                  <span className="info-label">Campus:</span>
+                  <span className="info-value">{campus ? campus.name : 'N/A'}</span>
+                </div>
+              </span>
+            </div>
+
         );
       })}
       <Link to="/addstudent" className="addstudent-link">
         Add Student
       </Link>
+    </div>
     </div>
   );
 };
