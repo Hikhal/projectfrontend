72d58d13e79367c1bf74dab4650bfdb6e3e092d6
Ghulam Nabi <ghualmnabia75@gmail.com>
2023-07-03 03:15:26 -0400

    CSS testing done with Successful results

--- uniapp/src/components/Home.js
+++ uniapp/src/components/Home.js
@@ -6,9 +6,12 @@ const Home = () => {
     
     <div className="homepage">
       <h1>Welcome to Our University!</h1>
-      <p>Experience the excellence of education and discover talented students across various programs.</p>
-      <p>At Our University, we are committed to providing a nurturing and inclusive learning environment. Our dedicated faculty and staff are passionate about fostering intellectual growth and personal development.</p>
-      <p>Explore our website to learn more about our programs, admission process, and student life. We invite you to join our vibrant community and embark on an exciting educational journey.</p>
+      <h3>Experience the excellence of education and discover talented students across various programs.</h3>
+      <h3>At Our University, we are committed to providing a nurturing and inclusive learning environment. Our dedicated faculty and staff are passionate about fostering intellectual growth and personal development.</h3>
+      <h3>Explore our website to learn more about our programs, admission process, and student life. We invite you to join our vibrant community and embark on an exciting educational journey.</h3>
+      <br/>
+      <h3>Credits to Shoaib, Hamza, and Ghualm</h3>
+
     </div>
   );
 };
